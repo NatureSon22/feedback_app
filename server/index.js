f9cc63e894099770404aeb27ccdbd2649b5a448d
NatureSon22 <blendingworker@gmail.com>
2024-07-25 13:48:41 +0800

    configure client url

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "https://feedapp-mern.netlify.app", // Use environment variable for frontend URL
+    origin: "https://feedbackback-mern.netlify.app", // Use environment variable for frontend URL
     methods: ["GET", "POST"],
     credentials: true,
   },
